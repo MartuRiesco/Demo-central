e5e8f5a12d5d161fe9998dc840086624fe3de8c4
Martu Riesco <marturiesco.costa@hotmail.com>
2024-05-30 17:34:28 -0300

    logo

--- turnos/src/components/Logo.js
+++ turnos/src/components/Logo.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Logo() {
   return (
-    /* <img src='https://i.ibb.co/FxQL9rv/logo-app-reservas.png' alt='App Turnos'/> */
-    <img src="https://i.ibb.co/gZdkQnQ/ganesha-logo-app-200.png" alt="ganesha-logo-app-200" border="0" />
+    <img src='https://i.ibb.co/FxQL9rv/logo-app-reservas.png' alt='App Turnos'/> 
+   /*  <img src="https://i.ibb.co/gZdkQnQ/ganesha-logo-app-200.png" alt="ganesha-logo-app-200" border="0" /> */
     
   )
 }
